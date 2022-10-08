9c779727c4306d30d923b640b27e5dbab5f169ba
dd-kyky-ll <115216927+dd-kyky-ll@users.noreply.github.com>
2022-10-08 09:41:55 +0800

    pull commit

--- ems_phase1/src/GitTest.java
+++ ems_phase1/src/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello gittt ll!");
         System.out.println("hello gittt ll master test!");
         System.out.println("hello gittt ll push test!");
+        System.out.println("hello gittt ll pull test!");
     }
 }
