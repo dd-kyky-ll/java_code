4c827f0178d64d9eb9c409b8d9cab87f1f0c0282
zll <dong2200977@163.com>
2022-10-08 09:20:06 +0800

    ll commit2

--- ems_phase1/src/GitTest.java
+++ ems_phase1/src/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello gitt!");
         System.out.println("hello gittt ll!");
+        System.out.println("hello gittt ll test!");
     }
 }
